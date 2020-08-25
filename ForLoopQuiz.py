f6a2964b435036edd2a8a5deeda92e4d1dc1ad09
hsingh5 <tech.harjeet@gmail.com>
2020-08-25 11:26:35 -0400

    Second Commit

--- ForLoopQuiz.py
+++ ForLoopQuiz.py
@@ -1,6 +1,4 @@
 list=[int,float,"a","b","56",67,7,1,2]
 for item in list:
-    if type(item)==int and item>6:
+    if type(item) == int and item > 6:  # str(item).isnumeric(): We can also use type(item)==int
         print(item)
-    else:
-        print("")
