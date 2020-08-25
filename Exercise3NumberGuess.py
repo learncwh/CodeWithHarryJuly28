30f3f912b9d1dbe025419167e6def7470a667dc8
hsingh5 <tech.harjeet@gmail.com>
2020-08-25 11:57:26 -0400

    Second Commit

--- Exercise3NumberGuess.py
+++ Exercise3NumberGuess.py
@@ -1,15 +1,15 @@
-#welcome to the guessing game
-numguess=5
-attempt=5
-while(True):
-    x=int(input("Enter the number to be guessed:"))
-    attempt-=1
-    if attempt==0:
+# welcome to the guessing game
+numguess = 5  # number to be guessed
+attempt = 5  # number of attemts remainging
+while (True):
+    x = int(input("Enter the number to be guessed:"))
+    attempt -= 1
+    if attempt == 0:
         print("Sorry max attempts reached..GAME OVERRRRR")
         break
-    elif x==numguess:
+    elif x == numguess:
         print("Congratulations you have guessed it right")
-        print("No. of attempts remaning:",attempt)
+        print("No. of attempts remaning:", attempt)
         break
     elif x<numguess:
         print("You have guessed a smaller number")
